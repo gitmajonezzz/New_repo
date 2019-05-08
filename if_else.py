e6b1081116e3ec57aab1e6aa4b571327801b50a1
gitmajonezzz <48793462+gitmajonezzz@users.noreply.github.com>
2019-05-08 22:25:02 +0200

    Update if_else.py

--- if_else.py
+++ if_else.py
@@ -1,11 +1,6 @@
-password = 'spoko'
-if password == 'spoko':
-    print ('Access granted')
-else:
-    print ('Access denied')
-so now motherfucker, I'll type something
-and now again i type in something
-and local3
+a = 1
 
-and now master would like to say something, what do ya think about it?
-and again motherfuckers I'd like to say something here and now
+if a == 1:
+    print("a = 1")
+else:
+    print("a != 1)
